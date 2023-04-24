dda5be7fb351d167cfb9ec0e6777e440c88e5bce
Yohmontana <estebanjcarlo@gmail.com>
2023-04-23 20:00:54 -0500

    avance

---
+++ build_pokemon/src/Components/Main.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
+
+const Main=()=>{
+  const [pokeData,setPokedata]=useState([]);
+  const [loading,setLoading]=useState(true);
+  const[url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/")
+  const[nextUrl,setNextUrl]=useState();
+  const[prevUrl,setPrevUrl]=useState();
+  const[pokeDex, setPokeDex]=useState();
+
+  const pokeFun=async()=>{
+    setLoading(true)
+    const res=await axios.get(url);
+    //console.log(res.data.results)
+    setNextUrl(res.data.next);
+    setPrevUrl(res.data.previous);
+    getPokemon(res.data.results)
+    setLoading(false)
+    console.log(pokeData);
+  }
+  const getPokemon=async(res)=>{
+    res.map(async(item)=>{
+      const result=await axios.get(item.url)
+      //console.log(result.data)
+      setPokedata(state=>{
+        state=[...state,result.data]
+        state.sort((a,b)=>a.id>b.id?1:-1)
+        return state;
+      })
+    })
+  }
+
+  useEffect(()=>{
+    pokeFun();
+  },[url])
+  return(
+    <>
+      <div className="container">
+        <div className="left-content">
+          <Card pokemon={pokeData} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>            
+          <div className="btn-group">
+            <button>Previous</button>
+            <button>Next</button>
+          </div>   
+        </div>
+
+        <div className="right-content">
+          <Pokeinfo/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main;
